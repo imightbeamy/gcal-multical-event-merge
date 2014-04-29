f26923cb922ce95226a5f6adde975c04684caf87
Amy Ciavolino <amyciav@gmail.com>
2014-04-29 09:06:50 -0400

    Fix js lint warnings and bug, update manifest, add ugly icons

--- events.js
+++ events.js
@@ -1,39 +1,44 @@
-function EventMerger (key_function, clean_up_function) {
+'use strict';
+
+function EventMerger(key_function, clean_up_function) {
     this.makeKey = key_function;
     this.cleanUp = clean_up_function;
 }
 
 EventMerger.prototype = {
-    getEventSets: function($events) {
-    	var event_sets = {},
+    getEventSets: function ($events) {
+        var event_sets = {},
             makeKey = this.makeKey;
-    	$events.each(function() {
-    		var $event = $(this),
-                key = makeKey($event).replace(/\s/g,'');
-    		event_sets[key] = event_sets[key] || [];
-    		event_sets[key].push($event);
-    	});
-    	return event_sets;
+        $events.each(function () {
+            var $event = $(this),
+                key = makeKey($event).replace(/\s/g, '');
+            event_sets[key] = event_sets[key] || [];
+            event_sets[key].push($event);
+        });
+        return event_sets;
     },
     makeStripes: function (colors) {
         var gradient = "repeating-linear-gradient( 45deg,",
             pos = 0;
-        $.each(colors, function(i, c) {
+        $.each(colors, function (i, c) {
             gradient += c + " " + pos + "px,";
-            pos+=10;
+            pos += 10;
             gradient += c + " " + pos + "px,";
-        })
+        });
         gradient = gradient.slice(0, -1);
-        gradient += ")"
+        gradient += ")";
         return gradient;
     },
     mergeEvents: function (name, event_set) {
         if (event_set.length > 1) {
-            var colors = $.map(event_set, function(event) {
+            var colors = $.map(event_set, function (event) {
                 return $(event).css('background-color');
             });
+
             var keep = event_set.shift();
-            $(event_set).each(function() {$(this).remove()});
+            $(event_set).each(function () {
+                $(this).parent().remove();
+            });
 
             keep.css('background-image', this.makeStripes(colors));
             this.cleanUp(keep);
@@ -47,21 +52,20 @@ EventMerger.prototype = {
 
 /*****************************************************************************/
 
-function eventKey ($event) {
+function eventKey($event) {
     var event_name = $event.find('dd span').text(),
         event_time = $event.find('dt').text(),
         col = $event.parents('.tg-col-eventwrapper').attr('id');
     return event_name + event_time + col;
 }
 
-function cleanUp ($event) {
+function cleanUp($event) {
     var chip = $event.parents('.chip'),
-        left = Number(chip.css('left').replace(/[%px]*/g,''));
+        left = Number(chip.css('left').replace(/[%px]*/g, ''));
     chip.css('width', 100 - left + "%");
-    $event.parents('td').find('.chip-border').remove();
 }
 
-merger = new EventMerger(eventKey, cleanUp);
- $(document).on("DOMNodeInserted", ".tg-mainwrapper", function(e) {
+var merger = new EventMerger(eventKey, cleanUp);
+$(document).on("DOMNodeInserted", ".tg-mainwrapper", function () {
     merger.mergeSets($('dl'));
- });
+});
